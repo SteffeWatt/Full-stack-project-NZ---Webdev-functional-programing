5f4e30954aa1973cab1a882d504f5dd7af456ae5
Steffen W.G <steffengravdehaug@hotmail.no>
2022-09-23 22:11:18 +1200

    added all the tabs to the page, need to fix them up properly. made changes to javascript to prevent code duplication

--- Client/swat192.js
+++ Client/swat192.js
@@ -1,25 +1,51 @@
 
 let HomeSection = document.getElementById("Home_Tab");
 let LoginSection = document.getElementById("Login_Tab");
-let GuestBookSection = document.getElementById("GuestBook_Tab")
+let RegisterSection = document.getElementById("Register_Tab");
+let GuestBookSection = document.getElementById("GuestBook_Tab");
+let GameSection = document.getElementById("GameOfChess_Tab");
+let ShopSection = document.getElementById("Shop_Tab");
 
 
 
 function ChangeDivContentHome(){
+    HideAllContent();
     HomeSection.style.display = "block";
-    LoginSection.style.display = "none";
-    GuestBookSection.style.display = "none";
 }
 
 function ChangeDivContentLogin(){
-    HomeSection.style.display = "none";
+    HideAllContent();
     LoginSection.style.display = "block";
-    GuestBookSection.style.display = "none";
+}
+
+function ChangeDivContentRegister(){
+    HideAllContent();
+    RegisterSection.style.display = "block";
+}
+
+function ChangeDivContentAcademyShop(){
+    HideAllContent();
+    ShopSection.style.display = "block";
 }
 
 function ChangeDivContentGuestBook(){
+    HideAllContent();
+    GuestBookSection.style.display = "block";
+}
+
+function ChangeDivContentGameOfChess(){
+    HideAllContent();
+    GameSection.style.display = "block";
+}
+
+
+
+function HideAllContent(){
     HomeSection.style.display = "none";
     LoginSection.style.display = "none";
-    GuestBookSection.style.display = "block";
+    RegisterSection.style.display = "none";
+    ShopSection.style.display = "none";
+    GameSection.style.display = "none";
+    GuestBookSection.style.display = "none";
 }
 
